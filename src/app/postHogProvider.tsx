5c1384b292659da1fabdef5cfaafd5b5efc54515
Koen Rijpstra <k.rijpstra@gmail.com>
2025-06-01 18:11:09 +0200

    Update posthog-js dependency to version 1.249.0; refactor PostHogProvider initialization and remove PostHogPageView component to streamline tracking logic.

--- src/app/postHogProvider.tsx
+++ src/app/postHogProvider.tsx
@@ -1,24 +1,20 @@
 'use client'
 
-import PostHogPageView from '@/components/PostHogPageView'
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: '/ingest',
+    ui_host: 'https://us.posthog.com',
+    person_profiles: 'identified_only',
+    defaults: '2025-05-24',
+  })
+}
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: '/ingest',
-      ui_host: 'https://us.posthog.com',
-      person_profiles: 'identified_only',
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-      capture_pageleave: true, // Enable pageleave capture
-    })
-  }, [])
-
   return (
     <PHProvider client={posthog}>
-      <PostHogPageView />
       {children}
     </PHProvider>
   )
